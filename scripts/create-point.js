ee6b87f10448e2efb38fa609d138392204f56969
Arthur Cesarino <cesarinoarthur@gmail.com>
2020-06-03 19:24:14 -0300

    organizing create-point.js

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,38 +1,44 @@
-function populateUFs() {
-    const ufSelector = document.querySelector("select[name=uf]")
+//function to fill the selctors on html 
+function fillSelectors(url, selector) {
 
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-        .then(res => res.json())
-        .then(states => {
+    fetch(url)
+    .then(res => res.json())
+        .then(elements => {
+            elements.forEach(element => {
 
-            states.forEach(state => {
-
-                ufSelector.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+                selector.innerHTML += `<option value="${element.id}">${element.nome}</option>`
             })
         })
-}
+    }
+
+//function that use IBGE API for the cities of states
 function getCities(event){
     const citySelector = document.querySelector("select[name=city]")
     const stateInput = document.querySelector("[name=state]")
+
     const ufValue = event.target.value
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios` 
+    const citiesUrl = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios` 
+
     const indexOfSelectedState = event.target.selectedIndex
-    console.log(stateInput)
+
     stateInput.value = event.target.options[indexOfSelectedState].text
     citySelector.innerHTML = '<option value="">Selecione a Cidade</option>'
-    fetch(url)
-    .then(res => res.json())
-        .then(cities => {
-            cities.forEach(city => {
-
-                citySelector.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-            })
-        })
+    
+    fillSelectors(citiesUrl, citySelector)
     citySelector.disabled = false
 }
 
-populateUFs()
+//function to catch all the brazilians states (IBGE)
+function populateUFs() {
+    const ufSelector = document.querySelector("select[name=uf]")
+    const statesUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+
+    fillSelectors(statesUrl, ufSelector)
+}
+//Add function getCities to event "change" 
 document
     .querySelector("select[name=uf]")
     .addEventListener('change', getCities)
-    
+
+//start the code calling function populateUFs
+populateUFs()
